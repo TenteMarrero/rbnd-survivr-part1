dc27a3789132d95679f3a8b705099b03ef5f80e0
Tente Marrero <tente.marrero@gmail.com>
2016-06-26 16:21:01 +0100

    Aditional gem functions

--- lib/colorizr.rb
+++ lib/colorizr.rb
@@ -1,17 +1,25 @@
 class String
+  @colors = {
+    red: "31", 
+    green: "32", 
+    yellow: "33", 
+    blue: "34", 
+    pink: "35", 
+    light_blue: "94", 
+    white: "97", 
+    light_grey: "37", 
+    black: "30"
+  }
+  def self.colors
+    @colors.map {|color| color[0]}
+  end
+  def self.sample_colors
+    @colors.each do |color|
+      puts "This is #{color[0].to_s.send(color[0])}"
+    end
+  end
   def self.create_colors
-    colors = {
-      red: "31", 
-      green: "32", 
-      yellow: "33", 
-      blue: "34", 
-      pink: "35", 
-      light_blue: "94", 
-      white: "97", 
-      light_grey: "37", 
-      black: "30"
-    }
-    colors.each do |color|
+    @colors.each do |color|
       self.send(:define_method, "#{color[0]}") do
         "\e[#{color[1]}m" + self + "\e[0m"
       end
@@ -20,3 +28,5 @@ class String
 end
 
 String.create_colors
+String.colors
+String.sample_colors
